6ce97a6d7c9a192d10c121dd1d6babe211f421ca
Emeli Dral <emeli.dral@gmail.com>
2025-08-28 14:32:10 +0100

    updated examples for demo service dev

---
+++ demo_service/flask_main.py
@@ -0,0 +1,18 @@
+from classifier import Classifier
+from codecs import open
+import time
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+classifier = Classifier()
+
+@app.route("/predict", methods = ["POST", "GET"])
+def index_page(text = "", prediction_message = ""):
+    if request.method == "POST":
+        text = request.form["text"]
+        prediction_message = classifier.get_result_message(text)
+
+    return render_template('simple_page.html', text = text, prediction_message = prediction_message)
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 80, debug = True)
